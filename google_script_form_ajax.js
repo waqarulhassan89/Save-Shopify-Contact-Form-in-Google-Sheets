13d95aaef25c906d3fdfa12bcc8bc1b8b45f2609
Waqar Ul Hassan (Alex Brown) <45354170+waqarulhassan89@users.noreply.github.com>
2020-06-08 23:03:24 +0500

    Update google_script_form_ajax.js
    Updated line 4 so it only submit data to google sheet when form is submitted successfully. Previously we were triggering this even when user click on submit button.

--- google_script_form_ajax.js
+++ google_script_form_ajax.js
@@ -1,7 +1,7 @@
 var $form = $('form#contact_form'),
     url = 'https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec'
 
-$('#submit-form').on('click', function(e) {
+$('#contact_form').on('submit', function(e) { // updated this so it only submit data when form is submitted successfully. Previously we were triggering this even when user click on submit button.
   e.preventDefault(); // delete this is you want to send data to admin email address.
   var jqxhr = $.ajax({
     url: url,
